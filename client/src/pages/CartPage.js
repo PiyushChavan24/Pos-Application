b528ce9ffafc2d5217213d4ed72c018bbee8be3e
piyushchavan24 <piyushchavan24@gmail.com>
2024-06-22 18:19:10 +0530

    Quantity Plus and minus functionality

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -2,11 +2,30 @@
 
 import React from "react";
 import DefaultLayout from "../components/DefaultLayout";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Table } from "antd";
-import { DeleteOutlined } from "@ant-design/icons";
+import {
+ DeleteOutlined,
+ PlusCircleOutlined,
+ MinusCircleOutlined,
+} from "@ant-design/icons";
 
 function CartPage() {
+ const dispatch = useDispatch();
+ const increaseQuantity = (record) => {
+  dispatch({
+   type: "updateCart",
+   payload: { ...record, quantity: record.quantity + 1 },
+  });
+ };
+ const decreaseQuantity = (record) => {
+  if (record.quantity !== 1) {
+   dispatch({
+    type: "updateCart",
+    payload: { ...record, quantity: record.quantity - 1 },
+   });
+  }
+ };
  const { cartItems } = useSelector((state) => state.rootReducer);
  const columns = [
   { title: "Name", dataIndex: "name" },
@@ -21,6 +40,20 @@ function CartPage() {
   },
   {
    title: "Quantity",
+   dataIndex: "_id",
+   render: (id, record) => (
+    <div>
+     <PlusCircleOutlined
+      className="mx-3"
+      onClick={() => increaseQuantity(record)}
+     />
+     <b>{record.quantity}</b>
+     <MinusCircleOutlined
+      className="mx-3"
+      onClick={() => decreaseQuantity(record)}
+     />
+    </div>
+   ),
   },
   {
    title: "Action",
