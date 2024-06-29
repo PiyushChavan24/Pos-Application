5c9abaf72c43ff473896765784ea812065cb23fc
piyushchavan24 <piyushchavan24@gmail.com>
2024-06-29 19:17:31 +0530

    categorizing the item functionality

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -7,9 +7,27 @@ import { Col, Row } from "antd";
 import Item from "../components/Item";
 import "../resources/item.css";
 import { useDispatch } from "react-redux";
+
 function Homepage() {
  const [itemsData, setItemData] = useState([]);
  const dispatch = useDispatch();
+ const [selectedCategory, setSelectedCategory] = useState("fruits");
+
+ const categories = [
+  {
+   name: "fruits",
+   imageUrl: "https://example.com/fruits.jpg",
+  },
+  {
+   name: "vegetables",
+   imageUrl: "https://example.com/vegetables.jpg",
+  },
+  {
+   name: "meat",
+   imageUrl: "https://example.com/meat.jpg",
+  },
+ ];
+
  const getAllItems = () => {
   dispatch({ type: "showLoading" });
   axios
@@ -27,15 +45,30 @@ function Homepage() {
  useEffect(() => {
   getAllItems();
  }, []);
+
  return (
   <DefaultLayout>
-   <Row gutter={20}>
-    {/*gutter is used to apply spacee bteween column*/}
-    {itemsData.map((item) => (
-     <Col xs={24} lg={6} md={12} sm={6}>
-      <Item item={item} />
-     </Col>
+   <div className="d-flex">
+    {categories.map((category) => (
+     <div
+      onClick={() => setSelectedCategory(category.name)}
+      className={`d-flex category ${
+       selectedCategory == category.name && "selected-category"
+      }`}
+      key={category.name}>
+      <h4>{category.name}</h4>
+      <img src={category.imageUrl} height="60" width="80" alt={category.name} />
+     </div>
     ))}
+   </div>
+   <Row gutter={20}>
+    {itemsData
+     .filter((i) => i.category === selectedCategory)
+     .map((item) => (
+      <Col xs={24} lg={6} md={12} sm={6} key={item.id}>
+       <Item item={item} />
+      </Col>
+     ))}
    </Row>
   </DefaultLayout>
  );
